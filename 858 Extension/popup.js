9972d023767392a4352b243d10e34d247e1e58af
Adrian Miguel <atmiguel@mit.edu>
2012-12-09 02:42:29 -0500

    displaying url on page

--- 858 Extension/popup.js
+++ 858 Extension/popup.js
@@ -1,4 +1,10 @@
+var current_url;
 chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
-    alert(tab.url);       //<-- return the url
-    console.log(tab.title);     //<-- return the title
+    current_url = tab.url;       //<-- return the url
+	console.log(tab);
+	main();
 });
+var main = function () {
+	var display_url = document.getElementById("display_url");
+	display_url.innerHTML = current_url;
+}

aab06755851593ff220700a8b0266e4b643d5798
Adrian Miguel <atmiguel@mit.edu>
2012-12-09 02:06:26 -0500

    added basic plugin files

---
+++ 858 Extension/popup.js
@@ -0,0 +1,5 @@
+chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
+    console.log(tab.url);       //<-- return the url
+    console.log(tab.title);     //<-- return the title
+	console.log(tab);
+});

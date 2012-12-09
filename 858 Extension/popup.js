f0d558bd38d870dc0c350158947ebce4cb846646
Adrian Miguel <atmiguel@mit.edu>
2012-12-09 02:13:29 -0500

    added username and password

--- 858 Extension/popup.js
+++ 858 Extension/popup.js
@@ -1,5 +1,4 @@
 chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
     console.log(tab.url);       //<-- return the url
     console.log(tab.title);     //<-- return the title
-	console.log(tab);
 });

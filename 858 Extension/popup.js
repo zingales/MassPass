072714d148d85892903d98fdb2cb096d7f3cd080
Adrian Miguel <atmiguel@mit.edu>
2012-12-09 08:17:53 -0500

    got the form to submit to js

--- 858 Extension/popup.js
+++ 858 Extension/popup.js
@@ -5,6 +5,12 @@ chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the informatio
 	main();
 });
 var main = function () {
+	document.forms["masspass_form"]["onsubmit"] = handleSubmit;
 	var display_url = document.getElementById("display_url");
 	display_url.innerHTML = current_url;
+}
+
+var handleSubmit = function() {
+	alert("i just love penises so much.");
+	return false;
 }

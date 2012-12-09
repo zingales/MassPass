83dbf4b9d04ec62eaaf106058186fdd7812f1b26
Adrian Miguel <atmiguel@mit.edu>
2012-12-09 02:16:45 -0500

    fixed get url in .js

--- 858 Extension/popup.js
+++ 858 Extension/popup.js
@@ -1,4 +1,4 @@
 chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
-    console.log(tab.url);       //<-- return the url
+    alert(tab.url);       //<-- return the url
     console.log(tab.title);     //<-- return the title
 });

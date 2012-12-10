6fa17b4feb544b824b1ca0398ecf9a4fc96cada0
G-Air <zingales@mit.edu>
2012-12-10 04:04:10 -0500

    added additional password fields in html

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,14 +1,6 @@
 var domain;
 chrome.tabs.getSelected(null, function(tab) { //<-- "tab" has all the information
-    // current_url = tab.url;       //<-- return the urla
-    console.log(tab.url)
-    console.log(tab.url.replace('http://',''))
-    console.log(tab.url.replace('http://','').replace('https://',''))
-    console.log(tab.url.replace('http://','').replace('https://','').split(/[/?#]/))
-    // var urlParts = tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0].replace("www.","");
-	// domain = urlParts[0];
 	domain =  tab.url.replace('http://','').replace('https://','').split(/[/?#]/)[0].replace("www.","");
-	console.log(tab);
 	main();
 });
 var main = function () {
@@ -18,13 +10,11 @@ var main = function () {
 }
 
 var handleSubmit = function(event) {
-	// alert("i just love penises so much.");
-	console.log(event);
 	var form = event.srcElement;
 	var password = form["password"];
 	var username = form["username"];
 	console.log("password "+password.value+" username "+username.value+ " domain "+domain);
-	// alert("password "+password+" username "+username);
-	// console.log(form.elements);
+	alert("password "+password.value+" username "+username.value+ " domain "+domain);
 	return false;
 }
+
